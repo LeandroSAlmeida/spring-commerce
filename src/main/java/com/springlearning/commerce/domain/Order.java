15a6212175a552ba24da5f78d7bed1b33005dc26
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2023-12-18 20:27:33 -0300

    Payment

--- src/main/java/com/springlearning/commerce/domain/Order.java
+++ src/main/java/com/springlearning/commerce/domain/Order.java
@@ -16,6 +16,8 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -57,5 +59,13 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 }
