f0c4a700ddc566dcb9fee5c18b906c86aa376bbd
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2023-12-20 15:19:46 -0300

    ManytoMany Category

--- src/main/java/com/springlearning/commerce/domain/Category.java
+++ src/main/java/com/springlearning/commerce/domain/Category.java
@@ -1,8 +1,19 @@
 package com.springlearning.commerce.domain;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -26,5 +37,9 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 }
