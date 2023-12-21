7f3d553a342586709960c811676fb407dbafde0d
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2023-12-21 17:01:13 -0300

    Update Order, Product

--- src/main/java/com/springlearning/commerce/domain/Product.java
+++ src/main/java/com/springlearning/commerce/domain/Product.java
@@ -3,6 +3,7 @@ package com.springlearning.commerce.domain;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 @Entity
 @Table(name = "tb_product")
@@ -77,4 +78,10 @@ public class Product {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {return items.stream().map(x -> x.getOrder()).toList();}
 }
