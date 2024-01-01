dc1682eaca54d2dc0b962797864ef1c33fe8bbeb
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-01-01 04:13:16 -0300

    Product Controller

--- src/main/java/com/springlearning/commerce/controllers/ProductController.java
+++ src/main/java/com/springlearning/commerce/controllers/ProductController.java
@@ -1,8 +1,11 @@
 package com.springlearning.commerce.controllers;
 import com.springlearning.commerce.domain.Product;
+import com.springlearning.commerce.dto.ProductDTO;
 import com.springlearning.commerce.repositories.ProductRepository;
+import com.springlearning.commerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +16,11 @@ import java.util.Optional;
 @RequestMapping(value = "/products")
 public class ProductController {
     @Autowired
-    private ProductRepository repository;
-    @GetMapping
-
-    public String findById(){
-        Optional<Product> result = repository.findById(1L);
-        Product product = result.get();
-        return product.getName();
+    private ProductService service;
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return service.findById(id);
 
     }
-
-
 
 }
