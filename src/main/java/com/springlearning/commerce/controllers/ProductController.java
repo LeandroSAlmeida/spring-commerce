e0790bf4dc1f2609b5dc4b626396668580c5d733
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-01-02 19:28:14 -0300

    Get Products Paged

--- src/main/java/com/springlearning/commerce/controllers/ProductController.java
+++ src/main/java/com/springlearning/commerce/controllers/ProductController.java
@@ -1,15 +1,16 @@
 package com.springlearning.commerce.controllers;
-import com.springlearning.commerce.domain.Product;
+
 import com.springlearning.commerce.dto.ProductDTO;
-import com.springlearning.commerce.repositories.ProductRepository;
 import com.springlearning.commerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 
 @RestController
@@ -20,6 +21,12 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id){
         return service.findById(id);
+
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findByAll(Pageable pageable){
+        return service.findAll(pageable);
 
     }
 
