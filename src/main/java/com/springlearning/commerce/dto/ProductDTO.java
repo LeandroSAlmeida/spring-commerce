6a65d3bf31cc479d2bc1cc8bd863f6343785a0d2
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-02-06 20:00:06 -0300

    Fix validation

--- src/main/java/com/springlearning/commerce/dto/ProductDTO.java
+++ src/main/java/com/springlearning/commerce/dto/ProductDTO.java
@@ -3,10 +3,7 @@ package com.springlearning.commerce.dto;
 
 import com.springlearning.commerce.domain.Category;
 import com.springlearning.commerce.domain.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +17,7 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
